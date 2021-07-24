f81fcedd3001e244cf54d7c1301fbf633cec317d
AnastasiyaYatsenko <62345631+AnastasiyaYatsenko@users.noreply.github.com>
2021-07-24 15:17:54 +0300

    Add files via upload

--- django_quiz/urls.py
+++ django_quiz/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('', views.TestListView.as_view(), name='index'),
     path('add', views.CreateTestView.as_view(), name='create_test'),
     path('add_question', views.CreateQuestionView.as_view(), name='create_question'),
+    path('search', views.SearchTestView.as_view(), name='search'),
+    path('filter', views.FilterTestView.as_view(), name='run_test'),
     path('run_<test_id>', views.CreateTestrunView.as_view(), name='run_test'),
     path('admin/', admin.site.urls)
 ]
