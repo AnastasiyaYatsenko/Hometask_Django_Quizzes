9f6e3017a75e48297bc093bc66cb0842ef12665b
AnastasiyaYatsenko <62345631+AnastasiyaYatsenko@users.noreply.github.com>
2021-08-13 21:37:57 +0300

    filter, search & order

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
-from api.views import TestViewSet, QuestionViewSet, TestTotalViewSet, RetrieveUpdateDestroyAPIView, TestFirstThreeViewSet
+from api.views import TestViewSet, QuestionViewSet, TestTotalViewSet, TestRUDAPIView, TestFirstThreeViewSet, QuestionRUDAPIView
 
 router = routers.DefaultRouter()
 #router.register(r'dishes', TestViewSet)
@@ -9,11 +9,12 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path('', include(router.urls)),
     path('tests/', TestViewSet.as_view()),
+    re_path('^tests/(?P<title>.+)/$', TestViewSet.as_view()),
     path('questions/', QuestionViewSet.as_view()),
     path('total/', TestTotalViewSet.as_view()),
     path('total_three/', TestFirstThreeViewSet.as_view()),
-    path('tests/<int:pk>/', RetrieveUpdateDestroyAPIView.as_view()),
-    path('questions/<int:pk>/', RetrieveUpdateDestroyAPIView.as_view()),
+    path('tests/<int:pk>/', TestRUDAPIView.as_view()),
+    path('questions/<int:pk>/', QuestionRUDAPIView.as_view()),
     path('api-auth/',
          include('rest_framework.urls', namespace='rest_framework')),
 ]
--- api/views.py
+++ api/views.py
@@ -1,14 +1,28 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from quiz.models import Test, Question
 from api.serializers import TestSerializer, QuestionSerializer, TestTotalSerializer
-from rest_framework import mixins, generics
+from rest_framework import mixins, generics, filters
 
 
 class TestViewSet(mixins.ListModelMixin,
                   mixins.CreateModelMixin,
                   generics.GenericAPIView):
-    queryset = Test.objects.all()
+    # queryset = Test.objects.all()
     serializer_class = TestSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['title', 'created_at']
+    ordering = ['created_at']
+
+    def get_queryset(self):
+        queryset = Test.objects.all()
+        title = self.request.query_params.get('title')
+        total_users = self.request.query_params.get('total_users')
+        if total_users is not None:
+            queryset = queryset.filter(total_users=total_users)
+        if title is not None:
+            queryset = queryset.filter(title=title)
+        return queryset
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
@@ -48,10 +62,9 @@ class TestFirstThreeViewSet(mixins.ListModelMixin,
         return self.list(request, *args, **kwargs)
 
 
-class RetrieveUpdateDestroyAPIView(mixins.RetrieveModelMixin,
-                                   mixins.UpdateModelMixin,
-                                   mixins.DestroyModelMixin,
-                                   generics.GenericAPIView):
+class TestRUDAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Test.objects.all()
+    serializer_class = TestSerializer
 
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
@@ -64,3 +77,23 @@ class RetrieveUpdateDestroyAPIView(mixins.RetrieveModelMixin,
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
+
+
+class QuestionRUDAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+
